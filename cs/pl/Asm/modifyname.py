ed251486386feadee6896ebd934c984ce6337bb7
Slowist-Lee <3230103034@zju.edu.cn>
2025-01-01 15:39:55 +0800

    Deployed 291fe98 with MkDocs version: 1.6.1

--- cs/pl/Asm/modifyname.py
+++ cs/pl/Asm/modifyname.py
@@ -2,4 +2,5 @@ with open('D://MyRepository//notebook-publish/notebook/docs/cs/pl/Asm/sum.md', '
     lines = file.readlines()
 with open('D://MyRepository//notebook-publish/notebook/docs/cs/pl/Asm/sum.md', 'w', encoding='utf-8') as file:
     for line in lines:
-        file.write(line.rstrip('\n') + '  \n')
+        if not line.startswith('|'):
+            file.write(line.rstrip('\n') + '  \n')
