9715d30e2b9607e22b2ec61a9c5c4ce22a5cdfc0
Slowist-Lee <3230103034@zju.edu.cn>
2025-02-11 16:30:17 +0800

    auto commit

--- notebook/auto.ps1
+++ notebook/auto.ps1
@@ -1,5 +1,10 @@
+cd D:\MyRepository\notebook-publish\notebook
 git add .
 git commit -m "auto commit"
 git push origin master
 
 mkdocs gh-deploy
+
+Get-Process | Out-File "D:\MyRepository\notebook-publish\notebook\auto-log\process_log.txt"
+
+Get-Service | Out-File "D:\MyRepository\notebook-publish\notebook\auto-log\service_log.txt"
